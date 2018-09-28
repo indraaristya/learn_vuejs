baad3b460df6471b9b20248e6bd90870a811d69c
indraaristya <indraaristya98@gmail.com>
2018-09-29 00:16:04 +0700

    back in dashboard

--- src/views/AddBarang.vue
+++ src/views/AddBarang.vue
@@ -1,61 +1,60 @@
 <template>
-<v-app>
+  <v-app>
     <v-layout justify-center>
-        <v-flex xs12 sm6>
-          <!-- <v-toolbar-items class="hidden-sm-and-down"> -->
-            <div style="text-align: left">
+      <v-flex xs12 sm6>
+        <!-- <v-toolbar-items class="hidden-sm-and-down"> -->
+        <div style="text-align: left">
           <v-btn color="error" style="margin:10px" @click="$router.push({name: 'yudhasboard'})">Back</v-btn>
-            </div>
-            <br>
-            <h2 class="white--text"><strong>Barang</strong></h2>
-            
-              
-            <v-card>
-                <v-form v-model="valid" @keyup.enter.native="submit(namabaju,hargabaju,linkbaju)">
-                    <v-layout justify-center>
-                        <v-flex xs12 sm6>
-                            <v-text-field v-model="namabaju" :rules="namaRules" :counter="20" label="Nama Barang" required></v-text-field>
-                            <v-text-field v-model="hargabaju" :rules="hargaRules" label="Harga Barang" required></v-text-field>
-                            <!-- <v-text-field v-model="linkbaju" :rules="linkRules" label="Link Gambar Barang" required></v-text-field> -->
-                            <br>
-                            <p v-if="linkbaju != ''" class="green--text"> Photo ready to Submit! </p>
-                            <div>
-                                <input type="file" multiple accept="image/jpeg/png" @change="detectFiles($event.target.files)">
-                                <v-progress-linear class="progress-bar" :style="{ width: progressUpload + '%'}">{{ progressUpload }}%</v-progress-linear>
-                            </div>
-                            <v-btn :disabled="!valid" @click="submit(namabaju,hargabaju,linkbaju)">
-                                Submit
-                            </v-btn>
-                        </v-flex>
-                    </v-layout>
-                </v-form>
-            </v-card>
-            <v-data-table :headers="headers" :items="barang" hide-actions class="elevation-1">
-                <template slot="items" slot-scope="props">
-                    <td class="text-center">{{ props.item.namabaju }}</td>
-                    <td class="text-center">{{ props.item.hargabaju }}</td>
-                    <!-- <td class="text-xs-right">{{ props.item.linkbaju }}</td> -->
-                    <td>
-                        <v-btn depressed small color="error" @click="deletes(props.item.id)">Delete</v-btn>
-                    </td>
-                </template>
+        </div>
+        <br>
+        <h2 class="white--text"><strong>Barang</strong></h2>
 
-            </v-data-table>
-            <v-progress-linear :indeterminate="true" v-if="barang[0] === undefined"></v-progress-linear>
-            <!-- <v-card>
+        <v-card>
+          <v-form v-model="valid" @keyup.enter.native="submit(namabaju,hargabaju,linkbaju)">
+            <v-layout justify-center>
+              <v-flex xs12 sm6>
+                <v-text-field v-model="namabaju" :rules="namaRules" :counter="20" label="Nama Barang" required></v-text-field>
+                <v-text-field v-model="hargabaju" :rules="hargaRules" label="Harga Barang" required></v-text-field>
+                <!-- <v-text-field v-model="linkbaju" :rules="linkRules" label="Link Gambar Barang" required></v-text-field> -->
+                <br>
+                <p v-if="linkbaju != ''" class="green--text"> Photo ready to Submit! </p>
+                <div>
+                  <input type="file" multiple accept="image/jpeg/png" @change="detectFiles($event.target.files)">
+                  <v-progress-linear class="progress-bar" :style="{ width: progressUpload + '%'}">{{ progressUpload }}%</v-progress-linear>
+                </div>
+                <v-btn :disabled="!valid" @click="submit(namabaju,hargabaju,linkbaju)">
+                  Submit
+                </v-btn>
+              </v-flex>
+            </v-layout>
+          </v-form>
+        </v-card>
+        <v-data-table :headers="headers" :items="barang" hide-actions class="elevation-1">
+          <template slot="items" slot-scope="props">
+            <td class="text-center">{{ props.item.namabaju }}</td>
+            <td class="text-center">{{ props.item.hargabaju }}</td>
+            <!-- <td class="text-xs-right">{{ props.item.linkbaju }}</td> -->
+            <td>
+              <v-btn depressed small color="error" @click="deletes(props.item.id)">Delete</v-btn>
+            </td>
+          </template>
+
+        </v-data-table>
+        <v-progress-linear :indeterminate="true" v-if="barang[0] === undefined"></v-progress-linear>
+        <!-- <v-card>
         <v-flex v-for="(namaB, idx) in namaBaju" :key="idx" xs4>
             <p> {{namaB.namabaju}} </p>
             <p> {{namaB.hargabaju}} </p>
         </v-flex>
         </v-card> -->
-        </v-flex>
+      </v-flex>
     </v-layout>
-    </v-app>
+  </v-app>
 </template>
 
 
 <script>
-import { db, storage} from "../main";
+import { db, storage } from "../main";
 export default {
   data: () => ({
     valid: true,
@@ -118,7 +117,7 @@ export default {
         "state_changed",
         sp => {
           this.progressUpload = Math.floor(
-            sp.bytesTransferred / sp.totalBytes * 100
+            (sp.bytesTransferred / sp.totalBytes) * 100
           );
         },
         null,
